d8ebf0c25ddcc6d2948525cdf747aa14e1e8058a
tony-sn <tony.sn@outlook.com>
2023-09-18 09:20:06 +1000

    Revert "chore: migrate prisma db"
    This reverts commit 59515fabf5d9c2ea7c1105a8944f7fb081dacb2a.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
-  imports: [
-    DevtoolsModule.register({
-      http: process.env.NODE_ENV !== 'production',
-    }),
-    AuthModule,
-  ],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    snapshot: true,
-  });
-  await app.listen(8080);
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
 }
 bootstrap();
